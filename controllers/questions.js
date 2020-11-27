338a81e28841984359377b60a034932cd8512995
Bryan Romero <bryanromerou@gmail.com>
2020-11-27 14:32:28 -0800

    added crud for question model

--- controllers/questions.js
+++ controllers/questions.js
@@ -1,32 +1,80 @@
 const db = require("../models");
 
+
 const index = (req,res)=>{
     db.Question.find({}).then((foundQuestions)=>{
+
         res.json({questions: foundQuestions})
+    
     }).catch((err)=>{
+
         console.log('Error in question.index', err);
         res.json({Error: 'Unable to get your data'})
-    });
 
-    // res.send("Now sending all of the question data");
+    });
 };
+
 
 const show = (req,res)=>{
-    res.send("now sending data from specific id");
+    db.Question.findById(req.params.id).then((foundQuestion)=>{
+
+        res.json({question: foundQuestion})
+
+    }).catch((err)=>{
+
+        console.log('Error in question.show', err);
+        res.json({Error: 'Unable to get your data'})
+
+    });
 };
+
 
 const create = (req,res) =>{
     db.Question.create(req.body).then((savedQuestion)=>{
+
         res.status(201).json({question: savedQuestion});
+
     }).catch((err)=>{
+
         console.log('Error in question.create', err);
         res.json({Error: 'Unable to submit your data'})
+
     });
-}
+};
+
+
+const update = (req,res)=>{
+    db.Question.findByIdAndUpdate(
+        req.params.id,
+        {$set: req.body},// This is set to {$set: req.body} instead if just req.body because we don't want to override the whole object we only want to override the field coming from the request body
+        { new: true}).then((updatedQuestion)=>{
+
+            res.json({question: updatedQuestion})
+
+        }).catch((err)=>{
+
+            console.log('Error in question.update', err);
+            res.json({Error: 'Unable to Update data'})
+
+        });
+    // res.send("updating data");
+};
+
+
+const destroy = (req,res)=>{
+    db.Question.findByIdAndDelete(req.params.id).then((deletedQuestion)=>{
+        res.json({question: deletedQuestion})
+    }).catch((err)=>{
+        console.log('Error in question.destroy', err);
+        res.json({Error: 'Unable to Delete data'})
+    });
+};
 
 
 module.exports = {
     index,
     show,
     create,
+    update,
+    destroy
 }
--- routes/questions.js
+++ routes/questions.js
@@ -6,5 +6,7 @@ const ctrl = require("../controllers");
 router.get("/",ctrl.questions.index);
 router.get("/:id",ctrl.questions.show);
 router.post("/",ctrl.questions.create);
+router.put("/:id",ctrl.questions.update);
+router.delete("/:id",ctrl.questions.destroy);
 
 module.exports = router;
